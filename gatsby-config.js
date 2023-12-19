10172189654d9f31d12b13aa32be1a8cc96d2d5f
Paul Abrams <john.paul.abrams@gmail.com>
2023-12-19 14:45:08 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Paul's Portentous Publication`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
